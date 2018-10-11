e0a6033425c3dfb245b90200611854777c7e7711
Zachary Tang <zacharytang.tc@gmail.com>
2018-10-11 12:32:35 +0800

    updated indentation

--- listing_detail.php
+++ listing_detail.php
@@ -1,168 +1,168 @@
 <!DOCTYPE html>
 <html lang="en" class="gr__preview_uideck_com"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/line-icons.css">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/slicknav.css">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/nivo-lightbox.css">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/animate.css">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/owl.carousel.css">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/main.css">
-
-<link rel="stylesheet" type="text/css" href="./assets/css/responsive.css">
-<link rel="stylesheet" id="colors" href="./assets/css/green.css" type="text/css"></head>
-<body data-gr-c-s-loaded="true">
-
-<?php
-include "./header.php";
-?>
-
-<div class="page-header" style="background: url(assets/img/banner1.jpg);">
-<div class="container">
-<div class="row">
-<div class="col-md-12">
-<div class="breadcrumb-wrapper">
-<h2 class="product-title">Item Details</h2>
-<ol class="breadcrumb">
-<li><a href="/ads-details.html#">Home /</a></li>
-<li class="current">Item Details</li>
-</ol>
-</div>
-</div>
-</div>
-</div>
-</div>
-
-<div class="section-padding">
-<div class="container">
-
-<div class="product-info row">
-<div class="col-lg-7 col-md-12 col-xs-12">
-<div class="details-box ads-details-wrapper">
-<div id="owl-demo" class="owl-carousel owl-theme" style="opacity: 1; display: block;">
-<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 3810px; left: 0px; display: block;"><div class="owl-item" style="width: 635px;"><div class="item">
-<div class="product-img">
-<img class="img-fluid" src="./assets/img/img-1.jpg" alt="">
-</div>
-<span class="price">$1,550</span>
-</div></div><div class="owl-item" style="width: 635px;"><div class="item">
-<div class="product-img">
-<img class="img-fluid" src="./assets/img/img-2.jpg" alt="">
-</div>
-<span class="price">$1,550</span>
-</div></div><div class="owl-item" style="width: 635px;"><div class="item">
-<div class="product-img">
-<img class="img-fluid" src="./assets/img/img-3.jpg" alt="">
-</div>
-<span class="price">$1,550</span>
-</div></div></div></div>
-
-
-<div class="owl-controls clickable"><div class="owl-pagination"><a href="/ads-details.html#prev" class="prev-owl"></a><div class="owl-page active"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img1.jpg&quot;) center center / cover no-repeat;"></a></div><div class="owl-page"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img2.jpg&quot;) center center / cover no-repeat;"></a></div><div class="owl-page"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img3.jpg&quot;) center center / cover no-repeat;"></a></div><a href="/ads-details.html#next" class="next-owl"></a></div></div></div>
-</div>
-</div>
-<div class="col-lg-5 col-md-12 col-xs-12">
-<div class="details-box">
-<div class="ads-details-info">
-<h2>iMac 2018 with 256 SSD and 8GB RAM</h2>
-<p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum saepe suscipit debitis neque, laborum! Magni ducimus suscipit modi.</p>
-<div class="details-meta">
-<span><a href="/ads-details.html#"><i class="lni-alarm-clock"></i> 7 Jan, 10:10 pm</a></span>
-<span><a href="/ads-details.html#"><i class="lni-map-marker"></i> New York</a></span>
-<span><a href="/ads-details.html#"><i class="lni-eye"></i> 299 View</a></span>
-</div>
-<h4 class="title-small mb-3">Specification:</h4>
-<ul class="list-specification">
-<li> <i class="lni-check-mark-circle"></i> 256GB PCIe flash storage</li>
-<li> <i class="lni-check-mark-circle"></i> 2.7 GHz dual-core Intel Core i5</li>
-<li> <i class="lni-check-mark-circle"></i> Turbo Boost up to 3.1GHz</li>
-<li> <i class="lni-check-mark-circle"></i> Intel Iris Graphics 6100</li>
-<li> <i class="lni-check-mark-circle"></i> 8GB memory</li>
-<li> <i class="lni-check-mark-circle"></i> 10 hour battery life</li>
-<li> <i class="lni-check-mark-circle"></i> 13.3" Retina Display</li>
-<li> <i class="lni-check-mark-circle"></i> 1 Year international warranty</li>
-</ul>
-</div>
-<ul class="advertisement mb-4">
-<li>
-<p><strong>Type:</strong> <a href="/ads-details.html#">Electronics</a>, <a href="/ads-details.html#">For sale</a></p>
-</li>
-<li>
-<p><strong>Condition:</strong> New</p>
-</li>
-<li>
-<p><strong>Brand:</strong> <a href="/ads-details.html#">Apple</a></p>
-</li>
-</ul>
-<div class="ads-btn mb-4">
-<a href="/ads-details.html#" class="btn btn-common btn-reply"><i class="lni-envelope"></i> Email</a>
-<a href="/ads-details.html#" class="btn btn-common"><i class="lni-phone-handset"></i> 01154256643</a>
-</div>
-</div>
-</div>
-</div>
-
-
-<div class="description-info">
-<div class="row">
-<div class="col-lg-8 col-md-6 col-xs-12">
-<div class="description">
-<h4>Description</h4>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-xs-12">
-<div class="short-info">
-<h4>Short Info</h4>
-<ul>
-<li><a href="/ads-details.html#"><i class="lni-users"></i> More ads by <span>User</span></a></li>
-<li><a href="/ads-details.html#"><i class="lni-printer"></i> Print this ad</a></li>
-<li><a href="/ads-details.html#"><i class="lni-reply"></i> Send to a friend</a></li>
-<li><a href="/ads-details.html#"><i class="lni-warning"></i> Report this ad</a></li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-
-</div>
-</div>
-
-<?php
-include "./footer.php";
-?>
-
-<a href="/ads-details.html#" class="back-to-top" style="display: none;">
-<i class="lni-chevron-up"></i>
-</a>
-
-<div id="preloader" style="display: none;">
-<div class="loader" id="loader-1"></div>
-</div>
-
-
-<script src="./assets/js/jquery-min.js"></script>
-<script src="./assets/js/popper.min.js"></script>
-<script src="./assets/js/bootstrap.min.js"></script>
-<script src="./assets/js/jquery.counterup.min.js"></script>
-<script src="./assets/js/waypoints.min.js"></script>
-<script src="./assets/js/wow.js"></script>
-<script src="./assets/js/owl.carousel.min.js"></script>
-<script src="./assets/js/nivo-lightbox.js"></script>
-<script src="./assets/js/jquery.slicknav.js"></script>
-<script src="./assets/js/main.js"></script>
-<script src="./assets/js/form-validator.min.js"></script>
-<script src="./assets/js/contact-form-script.min.js"></script>
-<script src="./assets/js/summernote.js"></script>
-
-</body></html>
+  
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/line-icons.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/slicknav.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/nivo-lightbox.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/animate.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/owl.carousel.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/responsive.css">
+  <link rel="stylesheet" id="colors" href="./assets/css/green.css" type="text/css"></head>
+  <body data-gr-c-s-loaded="true">
+    
+    <?php
+    include "./header.php";
+    ?>
+    
+    <div class="page-header" style="background: url(assets/img/banner1.jpg);">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="breadcrumb-wrapper">
+              <h2 class="product-title">Item Details</h2>
+              <ol class="breadcrumb">
+                <li><a href="/ads-details.html#">Home /</a></li>
+                <li class="current">Item Details</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="section-padding">
+      <div class="container">
+        
+        <div class="product-info row">
+          <div class="col-lg-7 col-md-12 col-xs-12">
+            <div class="details-box ads-details-wrapper">
+              <div id="owl-demo" class="owl-carousel owl-theme" style="opacity: 1; display: block;">
+                <div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 3810px; left: 0px; display: block;"><div class="owl-item" style="width: 635px;"><div class="item">
+                  <div class="product-img">
+                    <img class="img-fluid" src="./assets/img/img-1.jpg" alt="">
+                  </div>
+                  <span class="price">$1,550</span>
+                </div></div><div class="owl-item" style="width: 635px;"><div class="item">
+                  <div class="product-img">
+                    <img class="img-fluid" src="./assets/img/img-2.jpg" alt="">
+                  </div>
+                  <span class="price">$1,550</span>
+                </div></div><div class="owl-item" style="width: 635px;"><div class="item">
+                  <div class="product-img">
+                    <img class="img-fluid" src="./assets/img/img-3.jpg" alt="">
+                  </div>
+                  <span class="price">$1,550</span>
+                </div></div></div></div>
+                
+                
+                <div class="owl-controls clickable"><div class="owl-pagination"><a href="/ads-details.html#prev" class="prev-owl"></a><div class="owl-page active"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img1.jpg&quot;) center center / cover no-repeat;"></a></div><div class="owl-page"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img2.jpg&quot;) center center / cover no-repeat;"></a></div><div class="owl-page"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img3.jpg&quot;) center center / cover no-repeat;"></a></div><a href="/ads-details.html#next" class="next-owl"></a></div></div></div>
+              </div>
+            </div>
+            <div class="col-lg-5 col-md-12 col-xs-12">
+              <div class="details-box">
+                <div class="ads-details-info">
+                  <h2>iMac 2018 with 256 SSD and 8GB RAM</h2>
+                  <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum saepe suscipit debitis neque, laborum! Magni ducimus suscipit modi.</p>
+                  <div class="details-meta">
+                    <span><a href="/ads-details.html#"><i class="lni-alarm-clock"></i> 7 Jan, 10:10 pm</a></span>
+                    <span><a href="/ads-details.html#"><i class="lni-map-marker"></i> New York</a></span>
+                    <span><a href="/ads-details.html#"><i class="lni-eye"></i> 299 View</a></span>
+                  </div>
+                  <h4 class="title-small mb-3">Specification:</h4>
+                  <ul class="list-specification">
+                    <li> <i class="lni-check-mark-circle"></i> 256GB PCIe flash storage</li>
+                    <li> <i class="lni-check-mark-circle"></i> 2.7 GHz dual-core Intel Core i5</li>
+                    <li> <i class="lni-check-mark-circle"></i> Turbo Boost up to 3.1GHz</li>
+                    <li> <i class="lni-check-mark-circle"></i> Intel Iris Graphics 6100</li>
+                    <li> <i class="lni-check-mark-circle"></i> 8GB memory</li>
+                    <li> <i class="lni-check-mark-circle"></i> 10 hour battery life</li>
+                    <li> <i class="lni-check-mark-circle"></i> 13.3" Retina Display</li>
+                    <li> <i class="lni-check-mark-circle"></i> 1 Year international warranty</li>
+                  </ul>
+                </div>
+                <ul class="advertisement mb-4">
+                  <li>
+                    <p><strong>Type:</strong> <a href="/ads-details.html#">Electronics</a>, <a href="/ads-details.html#">For sale</a></p>
+                  </li>
+                  <li>
+                    <p><strong>Condition:</strong> New</p>
+                  </li>
+                  <li>
+                    <p><strong>Brand:</strong> <a href="/ads-details.html#">Apple</a></p>
+                  </li>
+                </ul>
+                <div class="ads-btn mb-4">
+                  <a href="/ads-details.html#" class="btn btn-common btn-reply"><i class="lni-envelope"></i> Email</a>
+                  <a href="/ads-details.html#" class="btn btn-common"><i class="lni-phone-handset"></i> 01154256643</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          
+          <div class="description-info">
+            <div class="row">
+              <div class="col-lg-8 col-md-6 col-xs-12">
+                <div class="description">
+                  <h4>Description</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6 col-xs-12">
+                <div class="short-info">
+                  <h4>Short Info</h4>
+                  <ul>
+                    <li><a href="/ads-details.html#"><i class="lni-users"></i> More ads by <span>User</span></a></li>
+                    <li><a href="/ads-details.html#"><i class="lni-printer"></i> Print this ad</a></li>
+                    <li><a href="/ads-details.html#"><i class="lni-reply"></i> Send to a friend</a></li>
+                    <li><a href="/ads-details.html#"><i class="lni-warning"></i> Report this ad</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      
+      <?php
+      include "./footer.php";
+      ?>
+      
+      <a href="/ads-details.html#" class="back-to-top" style="display: none;">
+        <i class="lni-chevron-up"></i>
+      </a>
+      
+      <div id="preloader" style="display: none;">
+        <div class="loader" id="loader-1"></div>
+      </div>
+      
+      
+      <script src="./assets/js/jquery-min.js"></script>
+      <script src="./assets/js/popper.min.js"></script>
+      <script src="./assets/js/bootstrap.min.js"></script>
+      <script src="./assets/js/jquery.counterup.min.js"></script>
+      <script src="./assets/js/waypoints.min.js"></script>
+      <script src="./assets/js/wow.js"></script>
+      <script src="./assets/js/owl.carousel.min.js"></script>
+      <script src="./assets/js/nivo-lightbox.js"></script>
+      <script src="./assets/js/jquery.slicknav.js"></script>
+      <script src="./assets/js/main.js"></script>
+      <script src="./assets/js/form-validator.min.js"></script>
+      <script src="./assets/js/contact-form-script.min.js"></script>
+      <script src="./assets/js/summernote.js"></script>
+      
+    </body></html>
