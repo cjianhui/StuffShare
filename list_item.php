b0f0ee2ec0a385541edd24060d97a7d3704641d2
Eldric <eldriclim93@gmail.com>
2018-10-11 23:30:32 +0800

    Fix redirect after creating item

--- list_item.php
+++ list_item.php
@@ -34,19 +34,19 @@
 
     $start_time = $_POST['bid_start'] === date("Y-m-d") ? date("Y-m-d H:i:s") : pg_escape_string($connection, $_POST['bid_start'])." 00:00:00";
     $end_time = pg_escape_string($connection, $_POST['bid_end'])." 23:59:59";
-    
+
     $borrow_duration = $_POST['borrow_duration'];
     $price = $_POST['price'];
     $address = pg_escape_string($connection, $_POST['address']);
     $desc = pg_escape_string($connection, $_POST['desc']);
-    
+
     $time_created = date("Y-m-d H:i:s");
-    
+
     $target_dir = './assets/img/items/';
     $image_file_type = strtolower(pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION));
     $img_name = time() . '.' . $image_file_type;
     $target_dest = $target_dir . $img_name;
-    
+
     if ($end_time <= $start_time){
       $date_error_msg = "<div class='alert alert-danger text-center'><strong>Start date cannot be after end date!</strong> </div>";
     } else if ($_FILES["file"]["size"] > 500000){
@@ -56,7 +56,7 @@
     } else {
       if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dest)) {
         // echo("File has been uploaded to " . $target_dest);
-        $query = "INSERT INTO item(item_name, time_created, start_price, bid_start, bid_end, 
+        $query = "INSERT INTO item(item_name, time_created, start_price, bid_start, bid_end,
                                   type, description, img_src, borrow_duration, address, username)
                   VALUES('".$item_name."','".$time_created."','".$price."','".$start_time."','".$end_time."',
                         '".$category."','".$desc."','".$img_name."','".$borrow_duration."','".$address."',
@@ -64,7 +64,12 @@
         // echo($query);
         $result = pg_query($connection,$query);
         $success_message = "<div class='alert alert-success text-center'><strong>Listing Created!</strong> Redirecting.. </div>";
-        header("refresh:2; url=./listing_detail.php");
+
+        $query_input = "SELECT item_id FROM item WHERE username='".$username."' AND time_created >= ALL(SELECT time_created FROM item WHERE username='".$username."')";
+        $input_result = pg_query($connection,$query_input);
+        $row = pg_fetch_assoc($input_result);
+
+        header("refresh:2; url=./listing_detail.php?id=".$row['item_id']);
       } else {
         echo("File upload error");
       }
@@ -96,11 +101,11 @@
 <div id="content" class="section-padding">
   <div class="container">
     <div class="row">
-      
+
       <?php
       include "user_sidebar.php";
       ?>
-      
+
       <div class="col-sm-12 col-md-8 col-lg-9">
         <div class="row page-content">
           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7">
@@ -110,7 +115,7 @@
                 <h2 class="dashbord-title">Item Details</h2>
               </div>
               <div class="dashboard-wrapper">
-                
+
                 <form name='item_form' method='post' enctype='multipart/form-data'>
                   <?php echo $date_error_msg;?>
                   <?php echo $file_size_error_msg;?>
@@ -134,7 +139,7 @@
                       </select>
                     </div>
                   </div>
-                  
+
                   <div class="form-group mb-3">
                     <label class="control-label">Bid Start</label>
                     <input class="form-control input-md" name="bid_start" required="" type="date" min=
@@ -143,37 +148,37 @@
                     ?>
                     >
                   </div>
-                  
+
                   <div class="form-group mb-3">
                     <label class="control-label">Bid End</label>
                     <input class="form-control input-md" name="bid_end" required="" type="date" min=
                       <?php
                         $date = strtotime("+1 day", strtotime("now"));
                         echo date("Y-m-d", $date);
-                      ?>  
+                      ?>
                     >
                   </div>
-                  
+
                   <div class="form-group mb-3">
                     <label class="control-label">Borrow Duration</label>
                     <input class="form-control input-md" name="borrow_duration" placeholder="Number Of Days" required="" type="number" min="1">
                   </div>
-                  
+
                   <div class="form-group mb-3">
                     <label class="control-label">Starting Bid</label>
                     <input class="form-control input-md" name="price" placeholder="Starting Bid" required="" type="number" min="0">
                   </div>
-                  
+
                   <div class="form-group mb-3">
                     <label class="control-label">Address</label>
                     <input class="form-control input-md" name="address" placeholder="Meet up location for transaction" type="text">
                   </div>
-                  
+
                   <div class="form-group mb-3">
                     <label class="control-label">Description</label>
                     <input class="form-control input-md" name="desc" placeholder="Describe your item" type="text">
                   </div>
-                  
+
                   <label class="tg-fileuploadlabel" for="tg-photogallery">
                     <span>Drop files anywhere to upload</span>
                     <span>Or</span>
