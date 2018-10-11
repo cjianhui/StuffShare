a2b2c2ef7ea9cb8d94e997fea932e2eeee2ff367
cjianhui <cjianhui1993@gmail.com>
2018-10-11 14:22:08 +0800

    Updated header.php to show username

--- header.php
+++ header.php
@@ -9,7 +9,12 @@
       <nav class="navbar navbar-expand-lg fixed-top scrolling-navbar">
         <div class="container">
           
-          <?php $current_page = $_SERVER['REQUEST_URI'] ?>
+          <?php
+          if (isset($_SESSION['key'])) {
+              $username = pg_escape_string($connection, $_SESSION['key']);
+          }
+          $current_page = $_SERVER['REQUEST_URI']
+          ?>
           
           <div class="navbar-header"><div class="slicknav_menu"><liner aria-haspopup="true" tabindex="0" class="slicknav_btn slicknav_collapsed" style=""><span class="slicknav_menutxt"></span><span class="slicknav_icon slicknav_no-text"><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span></span></liner><ul class="slicknav_nav slicknav_hidden" style="display: none;" aria-hidden="true" role="menu">
             <li>
@@ -25,7 +30,7 @@
               <a class = "<?php echo ($current_page == "/stuffshare/contact.html" ? "active" : "")?> " href="./contact.html" role="menuitem" tabindex="-1">Contact Us</a>
             </li>
             <li class="slicknav_collapsed slicknav_parent"><liner role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item slicknav_row" style="">
-              <a tabindex="-1">My Account</a>
+              <a tabindex="-1"><?php echo ($username ? $username : " My Account")?></a>
               <span class="slicknav_arrow"><i class="lni-chevron-right"></i></span></liner><ul class="dropdown slicknav_hidden" role="menu" style="display: none;" aria-hidden="true">
                 <li><a href="./user_home.php" role="menuitem" tabindex="-1"><i class="lni-home"></i> Account Home</a></li>
                 <li><a href="./user_listings.php" role="menuitem" tabindex="-1"><i class="lni-wallet"></i> My Listings</a></li>
@@ -70,7 +75,7 @@
             </ul>
             <ul class="sign-in">
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="lni-user"></i> My Account</a>
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="lni-user"></i><?php echo ($username ? $username : " My Account")?></a>
                 <div class="dropdown-menu">
                   <a class="dropdown-item" href="./user_home.php"><i class="lni-home"></i> Account Home</a>
                   <a class="dropdown-item" href="./user_listings.php"><i class="lni-wallet"></i> My Listings</a>
--- listings.php
+++ listings.php
@@ -20,8 +20,10 @@
 	<body data-gr-c-s-loaded="true">
 		
 		<?php
+      session_start();
       include "connect.php";
-      include './header.php';
+      include 'header.php';
+
       
       $page_size = 6;
       $num_pages_shown = 3;
