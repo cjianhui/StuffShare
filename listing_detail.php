721b3343a915d8bc46b12cdf84ab1f12429fa1af
Eldric <eldriclim93@gmail.com>
2018-10-11 14:02:27 +0800

    Add bidding feature

--- listing_detail.php
+++ listing_detail.php
@@ -22,9 +22,14 @@
 <link rel="stylesheet" id="colors" href="./assets/css/green.css" type="text/css"></head>
 <body data-gr-c-s-loaded="true">
 
-<?php
-include "./header.php";
-?>
+	<?php
+	 session_start();
+	 include "connect.php";
+	 if (!isset($_SESSION['key'])) {
+	     header("Location: ./login.php");
+	 }
+	 include "header.php";
+	?>
 
 <div class="page-header" style="background: url(assets/img/banner1.jpg);">
 <div class="container">
@@ -42,8 +47,53 @@ include "./header.php";
 </div>
 </div>
 
+<?php
+	$query_item = "SELECT * FROM item WHERE item_id=" . $_GET['id'];
+
+  $item_result = pg_query($connection,$query_item);
+	$row = pg_fetch_assoc($item_result);
+
+	$query_bids = "SELECT MAX(bid_amount) AS max_bid, COUNT(*) AS count FROM bid where item_id=" . $_GET['id'];
+	$bid_result = pg_query($connection,$query_bids);
+	$bids = pg_fetch_assoc($bid_result);
+
+	$curr_min_bid = empty($bids['max_bid']) ? $row['start_price'] : $bids[max_bid];
+?>
+
+<?php
+	$flash = "";
+	if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+		if(empty($_POST['bid_amt'])){
+			$flash = "<div class='alert alert-danger text-center' role='alert'>Invalid bid submitted!</div>";
+
+		} elseif ($_POST['bid_amt'] > $curr_min_bid) {
+			date_default_timezone_set('Asia/Singapore');
+
+			$insert_bid = "INSERT INTO bid VALUES(DEFAULT,'". date('d/m/Y H:i:s') . "',". $_POST['bid_amt'] .",'".$_SESSION['key']."',".$_GET['id'].")";
+			$write = pg_query($connection,$insert_bid);
+
+			if ($write){
+					$flash = "<div class='alert alert-success text-center' role='alert'>Bid of $". $_POST['bid_amt'] ." submitted!</div>";
+			} else {
+				$flash = "<div class='alert alert-danger text-center' role='alert'>Invalid format submitted!</div>";
+			}
+
+		} elseif ($_POST['bid_amt'] <= $curr_min_bid) {
+			$flash = "<div class='alert alert-danger text-center' role='alert'>Min. bid amount not met!</div>";
+
+		} else {
+			$flash = "<div class='alert alert-danger text-center' role='alert'>Invalid format submitted!</div>";
+
+		}
+	}
+?>
+
+
+
 <div class="section-padding">
 <div class="container">
+
+	<?=$flash?>
 
 <div class="product-info row">
 <div class="col-lg-7 col-md-12 col-xs-12">
@@ -51,91 +101,63 @@ include "./header.php";
 <div id="owl-demo" class="owl-carousel owl-theme" style="opacity: 1; display: block;">
 <div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 3810px; left: 0px; display: block;"><div class="owl-item" style="width: 635px;"><div class="item">
 <div class="product-img">
-<img class="img-fluid" src="./assets/img/img-1.jpg" alt="">
+<img class="img-fluid" src="./assets/img/items/<?=$row['img_src']?>" alt="">
 </div>
-<span class="price">$1,550</span>
-</div></div><div class="owl-item" style="width: 635px;"><div class="item">
-<div class="product-img">
-<img class="img-fluid" src="./assets/img/img-2.jpg" alt="">
-</div>
-<span class="price">$1,550</span>
-</div></div><div class="owl-item" style="width: 635px;"><div class="item">
-<div class="product-img">
-<img class="img-fluid" src="./assets/img/img-3.jpg" alt="">
-</div>
-<span class="price">$1,550</span>
+<span class="price">$<?=$curr_min_bid?>+</span>
 </div></div></div></div>
-
-
-<div class="owl-controls clickable"><div class="owl-pagination"><a href="/ads-details.html#prev" class="prev-owl"></a><div class="owl-page active"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img1.jpg&quot;) center center / cover no-repeat;"></a></div><div class="owl-page"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img2.jpg&quot;) center center / cover no-repeat;"></a></div><div class="owl-page"><span class=""></span><a class="item-link" style="background: url(&quot;assets/img/productinfo/img3.jpg&quot;) center center / cover no-repeat;"></a></div><a href="/ads-details.html#next" class="next-owl"></a></div></div></div>
 </div>
+</div>
+
 </div>
 <div class="col-lg-5 col-md-12 col-xs-12">
 <div class="details-box">
 <div class="ads-details-info">
-<h2>iMac 2018 with 256 SSD and 8GB RAM</h2>
-<p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum saepe suscipit debitis neque, laborum! Magni ducimus suscipit modi.</p>
+<h2><?=$row['item_name']?></h2>
 <div class="details-meta">
-<span><a href="/ads-details.html#"><i class="lni-alarm-clock"></i> 7 Jan, 10:10 pm</a></span>
-<span><a href="/ads-details.html#"><i class="lni-map-marker"></i> New York</a></span>
-<span><a href="/ads-details.html#"><i class="lni-eye"></i> 299 View</a></span>
+<span><i class="lni-alarm-clock"></i> <?=$row['borrow_duration']?> day</span>
+<span><i class="lni-map-marker"></i> <?=$row['address']?></span>
+<span><i class="lni-eye"></i> <?=$bids['count']?> bids</span>
 </div>
-<h4 class="title-small mb-3">Specification:</h4>
-<ul class="list-specification">
-<li> <i class="lni-check-mark-circle"></i> 256GB PCIe flash storage</li>
-<li> <i class="lni-check-mark-circle"></i> 2.7 GHz dual-core Intel Core i5</li>
-<li> <i class="lni-check-mark-circle"></i> Turbo Boost up to 3.1GHz</li>
-<li> <i class="lni-check-mark-circle"></i> Intel Iris Graphics 6100</li>
-<li> <i class="lni-check-mark-circle"></i> 8GB memory</li>
-<li> <i class="lni-check-mark-circle"></i> 10 hour battery life</li>
-<li> <i class="lni-check-mark-circle"></i> 13.3" Retina Display</li>
-<li> <i class="lni-check-mark-circle"></i> 1 Year international warranty</li>
-</ul>
-</div>
+
 <ul class="advertisement mb-4">
-<li>
-<p><strong>Type:</strong> <a href="/ads-details.html#">Electronics</a>, <a href="/ads-details.html#">For sale</a></p>
-</li>
-<li>
-<p><strong>Condition:</strong> New</p>
-</li>
-<li>
-<p><strong>Brand:</strong> <a href="/ads-details.html#">Apple</a></p>
-</li>
+<li><strong>Opening bid: </strong><?=$row['bid_start']?></li>
+<li><strong>Closing bid: </strong><?=$row['bid_end']?></li>
 </ul>
-<div class="ads-btn mb-4">
-<a href="/ads-details.html#" class="btn btn-common btn-reply"><i class="lni-envelope"></i> Email</a>
-<a href="/ads-details.html#" class="btn btn-common"><i class="lni-phone-handset"></i> 01154256643</a>
+</div>
+
+<div class="advertisement mb-4">
+<p><?=$row['description']?></p>
+</div>
+
+
+<div class="advertisement mb-2">
+  <form method="POST" action="listing_detail.php?id=<?=$row['item_id']?>">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text">$</span>
+        </div>
+				<input name="username" type="hidden" value="<?=$row['username']?>" />
+        <input name="bid_amt" type="number" class="form-control" min=<?=ceil($curr_min_bid)+1?> placeholder="<?=ceil($curr_min_bid)+1?> onwards">
+        <span class="input-group-btn">
+          <button class="btn btn-common btn-reply" type="submit">Bid It!</button>
+        </span>
+      </div><!-- /input-group -->
+    </form>
+</div>
+<div class="advertisement mb-2">
+
+<strong>more from</strong> <?=$row['username']?><strong>?</strong>
 </div>
 </div>
 </div>
 </div>
 
 
-<div class="description-info">
 <div class="row">
 <div class="col-lg-8 col-md-6 col-xs-12">
-<div class="description">
-<h4>Description</h4>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-</div>
-</div>
-<div class="col-lg-4 col-md-6 col-xs-12">
-<div class="short-info">
-<h4>Short Info</h4>
-<ul>
-<li><a href="/ads-details.html#"><i class="lni-users"></i> More ads by <span>User</span></a></li>
-<li><a href="/ads-details.html#"><i class="lni-printer"></i> Print this ad</a></li>
-<li><a href="/ads-details.html#"><i class="lni-reply"></i> Send to a friend</a></li>
-<li><a href="/ads-details.html#"><i class="lni-warning"></i> Report this ad</a></li>
-</ul>
-</div>
-</div>
 </div>
 </div>
 
-</div>
 </div>
 
 <?php
