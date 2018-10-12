9f723aafcdf156c608538aa56517d30991ba9539
marlenekoh <marlenekohxml@gmail.com>
2018-10-12 08:47:59 +0800

    manual merge rm magic numbers

--- listings.php
+++ listings.php
@@ -327,31 +327,31 @@
 	</div>
 </div>
 
-<?php
-	include 'footer.php';
-?>
+	<?php
+		include 'footer.php';
+	?>
 
 
-<a href="category.html#" class="back-to-top" style="display: none;">
-	<i class="lni-chevron-up"></i>
-</a>
+	<a href="category.html#" class="back-to-top" style="display: none;">
+		<i class="lni-chevron-up"></i>
+	</a>
 
-<div id="preloader" style="display: none;">
-	<div class="loader" id="loader-1"></div>
-</div>
+	<div id="preloader" style="display: none;">
+		<div class="loader" id="loader-1"></div>
+	</div>
 
 
-<script src="./assets/js/jquery-min.js"></script>
-<script src="./assets/js/popper.min.js"></script>
-<script src="./assets/js/bootstrap.min.js"></script>
-<script src="./assets/js/jquery.counterup.min.js"></script>
-<script src="./assets/js/waypoints.min.js"></script>
-<script src="./assets/js/wow.js"></script>
-<script src="./assets/js/owl.carousel.min.js"></script>
-<script src="./assets/js/nivo-lightbox.js"></script>
-<script src="./assets/js/jquery.slicknav.js"></script>
-<script src="./assets/js/main.js"></script>
-<script src="./assets/js/form-validator.min.js"></script>
-<script src="./assets/js/contact-form-script.min.js"></script>
+	<script src="./assets/js/jquery-min.js"></script>
+	<script src="./assets/js/popper.min.js"></script>
+	<script src="./assets/js/bootstrap.min.js"></script>
+	<script src="./assets/js/jquery.counterup.min.js"></script>
+	<script src="./assets/js/waypoints.min.js"></script>
+	<script src="./assets/js/wow.js"></script>
+	<script src="./assets/js/owl.carousel.min.js"></script>
+	<script src="./assets/js/nivo-lightbox.js"></script>
+	<script src="./assets/js/jquery.slicknav.js"></script>
+	<script src="./assets/js/main.js"></script>
+	<script src="./assets/js/form-validator.min.js"></script>
+	<script src="./assets/js/contact-form-script.min.js"></script>
 
-</body></html>
+	</body></html>
