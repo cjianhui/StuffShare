d2aa566a76e847ea7c7ad47a81b800e06f525bd5
Eldric <eldriclim93@gmail.com>
2018-11-08 11:36:06 +0800

    Fix drop table typo

--- sql/drop_table.sql
+++ sql/drop_table.sql
@@ -1,5 +1,5 @@
 DROP TRIGGER trigger_bid_insert on bid;
 DROP FUNCTION update_highest_bid();
-DROP TRIGGER bid;
-DROP TRIGGER item;
-DROP TRIGGER account;
+DROP TABLE bid;
+DROP TABLE item;
+DROP TABLE account;
