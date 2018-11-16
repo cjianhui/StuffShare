9c7c5207ca0f26b0f7a163f496d7cc28b41ef805
cjianhui <cjianhui1993@gmail.com>
2018-11-16 15:19:49 +0800

    Add config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/* Database credentials. Change accordingly to your settings. */
+#    $DB_SERVER = 'localhost';
+    $DB_SERVER = 'ec2-54-163-230-178.compute-1.amazonaws.com';
+    $DB_PORT = '5432';
+#   $DB_USERNAME = 'postgres';
+    $DB_USERNAME = 'dahdxfvzmrheaw';
+    $DB_PASSWORD = '8e58678a6a9d3eec9fdb8e057598f7bb9491a74091ed17b4b563420bc7fa7935';
+#   $DB_NAME = 'postgres';
+    $DB_NAME = 'daq2e7dfggt01s';
+?>
