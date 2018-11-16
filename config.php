78780aea1ae9ec3ce6eb8fd08d836b8addd4d05e
cjianhui <cjianhui1993@gmail.com>
2018-11-16 16:52:34 +0800

    Changed to get credentials from heroku

--- config.php
+++ config.php
@@ -1,11 +1,19 @@
 <?php
 /* Database credentials. Change accordingly to your settings. */
-#    $DB_SERVER = 'localhost';
-    $DB_SERVER = 'ec2-54-163-230-178.compute-1.amazonaws.com';
-    $DB_PORT = '5432';
+$url = getenv('DATABASE_URL');
+$pgstr = substr($url, 11, strlen($url));
+$tokens = preg_split('/:/', $pgstr);
+$DB_SERVER = preg_split('/@/', $tokens[1])[1];
+$DB_PORT = preg_split('/\//', $tokens[2])[0];
+$DB_USERNAME = $tokens[0];
+$DB_PASSWORD = preg_split('/@/', $tokens[1])[0];
+$DB_NAME = preg_split('/\//', $tokens[2])[1];
+
+/*
 #   $DB_USERNAME = 'postgres';
-    $DB_USERNAME = 'dahdxfvzmrheaw';
-    $DB_PASSWORD = '8e58678a6a9d3eec9fdb8e057598f7bb9491a74091ed17b4b563420bc7fa7935';
 #   $DB_NAME = 'postgres';
-    $DB_NAME = 'daq2e7dfggt01s';
+#   $DB_SERVER = 'localhost';
+#   $DB_USERNAME = 'postgres';
+#   $DB_NAME = 'postgres';
+*/
 ?>
